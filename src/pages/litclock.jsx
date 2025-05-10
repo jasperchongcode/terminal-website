2d6b34c1c3bbf842cb5a010648bcf5876dee7443
Jasper Chong <74451969+jasperchongcode@users.noreply.github.com>
2025-05-10 23:06:10 +1000

    add no-crt to hard to read

--- src/pages/litclock.jsx
+++ src/pages/litclock.jsx
@@ -1,6 +1,7 @@
 const LitClock = () => {
     return (
-        <div className="bg-white m-4">
+        <div className="bg-white m-4 no-crt">
+            <div className="text-black">(CRT effects disabled as difficult to read)</div>
             <meta charSet="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <title>lit-clock writeup</title>
